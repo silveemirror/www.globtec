8b889f9397de53676235ff06472179851f4143f7
silveemirror <148557899+silveemirror@users.noreply.github.com>
2023-10-22 20:06:00 +0200

    "Summary: GLOBTECH Gadgets is your gateway to a world of innovation in technology. We provide a handpicked selection of the latest tech products, seamless online shopping, secure payments, and responsive customer support. Join us to explore the forefront of tech innovation."
    "GLOBTECH Gadgets: Your destination for the latest in tech innovation. Discover a world of cutting-edge smartphones, smartwatches, gaming accessories, and smart home devices. We're committed to quality, reliability, and your seamless tech shopping experience. Join us in exploring the future of technology."

---
+++ JavaScript/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Gaming Items", "Android Devices", "Iphones", "Other Gadgets"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
